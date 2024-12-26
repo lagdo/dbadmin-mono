32b00947ad2a1e9156925918f21564c69bb7e95b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-12-26 02:08:41 +0100

    Annotations cache dir.

--- dbadmin-demo/jaxon.php
+++ dbadmin-demo/jaxon.php
@@ -2,4 +2,7 @@
 
 require(__DIR__ . '/../vendor/autoload.php');
 
+$sCacheDirKey = 'jaxon_annotations_cache_dir';
+Jaxon\jaxon()->di()->val($sCacheDirKey, __DIR__ . '/cache/annotations');
+
 Jaxon\jaxon()->app()->setup(__DIR__ . '/config.php');
