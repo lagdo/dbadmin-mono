1a86945ba11f10aebae4d062b93c83f8eb472f00
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-04-28 23:12:57 +0200

    Change the Jaxon annotations cache dir.

--- dbadmin-demo/jaxon.php
+++ dbadmin-demo/jaxon.php
@@ -3,6 +3,6 @@
 require(__DIR__ . '/../vendor/autoload.php');
 
 $sCacheDirKey = 'jaxon_annotations_cache_dir';
-Jaxon\jaxon()->di()->val($sCacheDirKey, __DIR__ . '/cache/annotations');
+Jaxon\jaxon()->di()->val($sCacheDirKey, '/var/cache/jaxon');
 
 Jaxon\jaxon()->app()->setup(__DIR__ . '/config.php');
