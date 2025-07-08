fb7d1e7aa5e31a1222679ffdf5738e0db3264f8d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-07-09 00:52:28 +0200

    Install the lagdo/facades package.

--- dbadmin-demo/jaxon.php
+++ dbadmin-demo/jaxon.php
@@ -1,8 +1,24 @@
 <?php
 
+use Lagdo\Facades\ContainerWrapper;
+use Psr\Container\ContainerInterface;
+
 require(__DIR__ . '/../vendor/autoload.php');
 
 $sCacheDirKey = 'jaxon_annotations_cache_dir';
-Jaxon\jaxon()->di()->val($sCacheDirKey, '/var/cache/jaxon');
+jaxon()->di()->val($sCacheDirKey, '/var/cache/jaxon');
+jaxon()->app()->setup(__DIR__ . '/config.php');
 
-Jaxon\jaxon()->app()->setup(__DIR__ . '/config.php');
+ContainerWrapper::setContainer(new class implements ContainerInterface
+    {
+        public function get($id)
+        {
+            return jaxon()->di()->g($id);
+        }
+
+        public function has($id): bool
+        {
+            return jaxon()->di()->h($id);
+        }
+    }
+);
