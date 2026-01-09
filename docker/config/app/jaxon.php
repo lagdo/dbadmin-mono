8d907b5a5310d817eef14710aabdc3ca02090dc2
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-01-09 23:33:31 +0100

    Use the SweetAlert dialog library.

--- docker/config/app/jaxon.php
+++ docker/config/app/jaxon.php
@@ -45,8 +45,8 @@ return [
         'dialogs' => [
             'default' => [
                 'modal' => 'bootbox',
-                'alert' => 'toastr',
-                'confirm' => 'noty',
+                'alert' => 'sweetalert',
+                'confirm' => 'sweetalert',
             ],
         ],
         'assets' => [
