decfd98ef6d2f134bdf4036f52ff5a3d0a4a1736
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-01-26 12:46:27 +0100

    Add the total option to the table query.

--- dbadmin-driver/src/Dto/TableSelectDto.php
+++ dbadmin-driver/src/Dto/TableSelectDto.php
@@ -8,60 +8,11 @@ use function implode;
 class TableSelectDto
 {
     /**
-     * The table name
-     *
-     * @var string
-     */
-    public $table = '';
-
-    /**
-     * The fields to select
-     *
-     * @var array
-     */
-    public $fields = [];
-
-    /**
-     * The where clauses
-     *
-     * @var array
-     */
-    public $where = [];
-
-    /**
-     * The group by clauses
-     *
-     * @var array
-     */
-    public $group = [];
-
-    /**
-     * The order by clauses
-     *
-     * @var array
-     */
-    public $order = [];
-
-    /**
      * All clauses, formatted
      *
      * @var string
      */
     public $clauses = '';
-
-    /**
-     * The row limit
-     *
-     * @var int
-     */
-    public $limit = 1;
-
-    /**
-     * The page number
-     *
-     * @var int
-     */
-    public $page = 0;
 
     /**
      * The constructor
@@ -74,16 +25,10 @@ class TableSelectDto
      * @param int $limit
      * @param int $page
      */
-    public function __construct(string $table, array $fields, array $where,
-        array $group, array $order = [], int $limit = 1, int $page = 0)
+    public function __construct(public string $table, public array $fields,
+        public array $where, public array $group, public array $order = [],
+        public int $limit = 1, public int $page = 0)
     {
-        $this->table = $table;
-        $this->fields = $fields;
-        $this->where = $where;
-        $this->group = $group;
-        $this->order = $order;
-        $this->limit = $limit;
-        $this->page = $page;
         if (!empty($where)) {
             $this->clauses = ' WHERE ' . implode(' AND ', $where);
         }
