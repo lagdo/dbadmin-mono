637932b17e23860f16daba46bdb2edba62a99a29
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-07-13 08:05:50 +0200

    Improve the Query Editor UI layout.

--- dbadmin-demo/app/config/foreigns.php
+++ dbadmin-demo/app/config/foreigns.php
@@ -18,6 +18,47 @@ return [
                         'joins' => ["INNER JOIN address ON store.address_id=address.address_id"],
                     ],
                 ],
+                'customer' => [
+                    'customer_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                        'search' => fn(string $search) => "\"first_name\" ILIKE $search OR \"last_name\" ILIKE $search",
+                    ],
+                ],
+                'staff' => [
+                    'staff_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                        'search' => fn(string $search) => "\"first_name\" ILIKE $search OR \"last_name\" ILIKE $search",
+                    ],
+                ],
+            ],
+        ],
+        'sakila' => [
+            'public' => [
+                'actor' => [
+                    'actor_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                        'search' => fn(string $search) => "\"first_name\" ILIKE $search OR \"last_name\" ILIKE $search",
+                    ],
+                ],
+                'store' => [
+                    'store_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(address.address, 1, $textLength)",
+                        'search' => fn(string $search) => "address.address ILIKE $search",
+                        'joins' => ["INNER JOIN address ON store.address_id=address.address_id"],
+                    ],
+                ],
+                'customer' => [
+                    'customer_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                        'search' => fn(string $search) => "\"first_name\" ILIKE $search OR \"last_name\" ILIKE $search",
+                    ],
+                ],
+                'staff' => [
+                    'staff_id' => [
+                        'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                        'search' => fn(string $search) => "\"first_name\" ILIKE $search OR \"last_name\" ILIKE $search",
+                    ],
+                ],
             ],
         ],
     ],
@@ -36,6 +77,26 @@ return [
                     'joins' => ["INNER JOIN address ON store.address_id=address.address_id"],
                 ],
             ],
+            'customer' => [
+                'customer_id' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
+                ],
+            ],
+            'staff' => [
+                'staff_id' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
+                ],
+            ],
+        ],
+        'employees' => [
+            'employees' => [
+                'emp_no' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
+                ],
+            ],
         ],
     ],
     'dbadmin-mysql' => [
@@ -51,6 +112,26 @@ return [
                     'select' => fn(int $textLength) => "SUBSTR(address.address, 1, $textLength)",
                     'search' => fn(string $search) => "LOWER(address.address) LIKE $search",
                     'joins' => ["INNER JOIN address ON store.address_id=address.address_id"],
+                ],
+            ],
+            'customer' => [
+                'customer_id' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
+                ],
+            ],
+            'staff' => [
+                'staff_id' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
+                ],
+            ],
+        ],
+        'employees' => [
+            'employees' => [
+                'emp_no' => [
+                    'select' => fn(int $textLength) => "SUBSTR(CONCAT(first_name, ' ', last_name), 1, $textLength)",
+                    'search' => fn(string $search) => "LOWER(first_name) LIKE $search OR LOWER(last_name) LIKE $search",
                 ],
             ],
         ],
