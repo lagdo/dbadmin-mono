95348fff681c51017e1429b75afe594a120f7b4a
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-09-04 11:38:45 +0200

    Fix the file export route.

--- dbadmin-demo/public/export.php
+++ dbadmin-demo/public/export.php
@@ -7,5 +7,4 @@ use Lagdo\DbAdmin\Support\Facade\FileSystem;
 // Set the content type
 header('Content-Type: text/plain');
 
-$fs = FileSystem::instance();
-echo !$fs ? "No export reader set." : $fs->read($_GET['file'] ?? '');
+echo FileSystem::instance()?->read($_GET['file'] ?? '') ?? 'No export reader set.';
